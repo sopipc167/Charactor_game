ed72776031daaa43758290080a189fd88606752c
Hotties <66089741+Hotties@users.noreply.github.com>
2020-11-27 17:18:41 +0900

    스택

--- SWdesign18/SWdesign18/ItemStack.c
+++ SWdesign18/SWdesign18/ItemStack.c
@@ -1,45 +1,6 @@
 #include"Stack.h"
 #include<string.h>
 
-int isFull()
-{
-	if (top + 1 == SIZE)
-		return 1;
-	else
-		return 0;
-}
-int isEmpty()
-{
-	if (top == -1)
-		return 1;
-	else
-		return 0;
-}
-void insert(int ascii)
-{
-	if (isFull())
-	{
-		//UI¿¡¼­ ²ËÃ¡´Ù´Â°Å Ç¥½Ã
-	}
-	else
-	{
-		top++;
-		stack[top] = ascii;
-		
-	}
-}
-void pop(int ascii)
-{
-	if (isEmpty())
-	{
-
-	}
-	else
-	{
-		stack[top] = '\0';
-		top--;
-	}
-}
 void isComplete(int ascii)
 {
 	int i, j=0;
